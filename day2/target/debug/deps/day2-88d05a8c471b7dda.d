17166b761f1b21e2e964f09d57ed76e97e784a4d
Abdul-Muiz-Iqbal <67752771+Abdul-Muiz-Iqbal@users.noreply.github.com>
2021-12-05 16:26:17 +0500

    Delete day2/target directory

--- day2/target/debug/deps/day2-88d05a8c471b7dda.d
+++
@@ -1,5 +0,0 @@
-/media/muiz/Files/code/AoC/2021/day2/target/debug/deps/day2-88d05a8c471b7dda.rmeta: src/main.rs
-
-/media/muiz/Files/code/AoC/2021/day2/target/debug/deps/day2-88d05a8c471b7dda.d: src/main.rs
-
-src/main.rs:
